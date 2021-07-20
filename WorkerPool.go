6c458023602ab1de11db544022b5f4975d786ece
jackie.chen <jackie.chen@coconutislandstudio.com>
2021-07-20 18:05:34 +0800

    线程等待

--- WorkerPool.go
+++ WorkerPool.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 /**
 工作池 类比于Java的线程池，通过协程 和管道，实现了任务的并行处理
 */
-func main() {
+func main1() {
 	const nums = 5
 	jobs := make(chan int, nums)
 	results := make(chan int, nums)
@@ -25,6 +26,9 @@ func main() {
 	for a := 1; a <= nums; a++ {
 		<-results
 	}
+
+	// 等待多个协程完成，我们可以使用 *wait group*
+
 }
 
 func gWorker(id int, jobs <-chan int, results chan<- int) {
@@ -34,4 +38,22 @@ func gWorker(id int, jobs <-chan int, results chan<- int) {
 		fmt.Println("worker", id, "finished job", j)
 		results <- j * 2
 	}
+}
+
+func waitWorker(id int, wg *sync.WaitGroup) {
+	defer wg.Wait()
+	fmt.Printf("worker %d starting \n\n", id)
+	time.Sleep(time.Second)
+	fmt.Printf("worker %d done \n", id)
+
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	for i := 1; i <= 5; i++ {
+		wg.Add(1)
+		go waitWorker(i, &wg)
+	}
+	wg.Wait()
 }
